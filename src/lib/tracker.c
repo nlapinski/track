902e860ae7e8b92bd395427e9cb20ba7cc2f17ff
nlapinski <nlapinski@Users-MacBook-Pro-2.local>
2021-03-19 16:15:44 -0700

    graphics and basic fucntions

--- src/lib/tracker.c
+++ src/lib/tracker.c
@@ -3,8 +3,9 @@
 #include "SDL_image.h"
 
 
-#define FONT_CW 8
-#define FONT_CH 8
+
+#define FONT_CW 12
+#define FONT_CH 12
 #define SSEQ_TRACKS 16
 
 //lots of globals @_@
@@ -13,11 +14,19 @@ static int sdlflags = SDL_HWSURFACE| SDL_FULLSCREEN ;	/* SDL display init flags 
 
 int die =0;	
 int last_tick; //last sdl tick
+int playing=0;
 static int tempo =120;
-
+static int pos=0;
+static int track = 0;
+static int emode =0;
 // gui stufff
 static SDL_Surface *font = NULL;
 static SDL_Surface *screen = NULL;
+
+static char tracks[SSEQ_TRACKS][1000];
+
+
+
 
 
 SDL_Surface *gui_load_image(const char *fn)
@@ -45,13 +54,13 @@ SDL_Surface *gui_load_image(const char *fn)
 int gui_open(SDL_Surface *scrn)
 {
 	//screen = scrn;
-	font = gui_load_image("font.png");
+	font = gui_load_image("font_1.5x.png");
 	if(!font)
 	{
 		fprintf(stderr, "Couldn't load font!\n");
 		return -1;
 	}
-	SDL_EnableKeyRepeat(250, 25);
+	SDL_EnableKeyRepeat(1, 1);
 	return 0;
 }
 
@@ -83,10 +92,13 @@ void gui_text(int x, int y, const char *txt, SDL_Surface *dst,int r, int g ,int 
 		///ughhh
 		//printf("%d=%d a=c\n",a,c-32);
 		//c=convert;
-		switch(c)
+		switch(a)
 		{
-		  case 0:	/* terminator */
-			break;
+		  case 0:
+		  	break;
+
+		  case '\e':
+		  	break;
 		  case '\n':	/* newline */
 			x = sx;
 			y += FONT_CH;
@@ -121,18 +133,31 @@ void gui_text(int x, int y, const char *txt, SDL_Surface *dst,int r, int g ,int 
 }
 
 
+void gui_rec()
+{
+
+	if(emode){
+		gui_text(220, 32, "**REC**", screen,255,0,199);
+	}
+	else{
+		gui_text(220, 32, "**REC**", screen,9,255,199);	
+	}
+}
+
 void gui_tempo(int v)
 {
 	char buf[32];
 	snprintf(buf, sizeof(buf), "  Tempo: %4.1d", v);
-	gui_text(12, 52, buf, screen,9,255,199);
+	gui_text(12, 32, buf, screen,9,255,199);
 }
 void gui_songedit(int pos, int ppos, int track, int editing)
 {
 	int t, n;
 	char buf[128];
+	char np[5];
+	char note[4];
 	SDL_Rect r;
-	const int y0 = 146;
+	const int y0 = 64;
 
 	/* Clear */
 	r.x = 12 - 2;
@@ -141,47 +166,80 @@ void gui_songedit(int pos, int ppos, int track, int editing)
 	r.h = FONT_CH * (SSEQ_TRACKS + 2) + 4 + 5;
 
 	/* Upper time bar */
-	snprintf(buf, sizeof(buf), "\027...\022...\022...\022..."
-			"\027...\022...\022...\022...");
+	//snprintf(buf, sizeof(buf), "\027...\022...\022...\022..."
+	//		"\027...\022...\022...\022...");
 	gui_text(12 + 6 * FONT_CW, y0, buf, screen,9,255,199);
 
 	/* Track names + cursor */
-	gui_text(12, y0 + FONT_CH + 3, "Trk00\nTrk01\nTrk02\nTrk03\n"
-			"Trk04\nTrk05\nTrk06\nTrk07\n"
-			"Trk08\nTrk09\nTrk10\nTrk11\n"
-			"Trk12\nTrk13\nTrk14\nTrk15",
-			screen,9,255,199);
-	gui_text(12, y0 + FONT_CH * (1 + track) + 3,
-			"\003\001\005", screen,9,255,199);
+	gui_text((FONT_CW*4), y0 - FONT_CH,"T-0  T-1  T-2  T-3  T-4  T-5  T-6  T-7  T-8  T-9  T-A  T-B  T-C  T-D  T-E  T-F",
+			screen,200,255,199);
+	
+	//???????????????
+	//gui_text(12, y0 + FONT_CH * (1 + track) + 3,
+	//		"\003\001\005", screen,9,255,199);
 
 	/* Lower time bar */
-	snprintf(buf, sizeof(buf), "\007%.4d\022...\007%.4d\022..."
-			"\007%.4d\022...\007%.4d\022...",
-			pos, pos + 8, pos + 16, pos + 24);
-	gui_text(12 + 6 * FONT_CW,
-			y0 + FONT_CH * (SSEQ_TRACKS + 1) + 6, buf, screen,9,255,199);
+	//snprintf(buf, sizeof(buf), "\007%.4d\022...\007%.4d\022..."
+	//		"\007%.4d\022...\007%.4d\022...",
+	//		pos, pos + 8, pos + 16, pos + 24);
+	//gui_text(12 + 6 * FONT_CW,
+	//		y0 + FONT_CH * (SSEQ_TRACKS + 1) + 6+190, buf, screen,9,255,199);
+
+
 
 	/* Notes */
 	buf[1] = 0;
-	for(t = 0; t < SSEQ_TRACKS; ++t)
+
+	for(t = 0; t < SSEQ_TRACKS; ++t){
+
+		if(playing && tracks[t][pos]){
+			printf("note on ->%c  track ->%d\n",tracks[t][pos],t);
+		};
+
 		for(n = 0; n < 32; ++n)
 		{
 			//int note = sseq_get_note(pos + n, t);
-			int note =35;
-			if(note < 0)
-				continue;
-			else
-				buf[0] = note;
-			gui_text(12 + FONT_CW * (6 + n),
-					y0 + FONT_CH * (1 + t) + 3,
-					buf, screen,9,255,199);
-		}
+			
+			//int note =35;
+			//char note = "----";
+			//int note = "-";
+			
+			if(tracks[t][pos+n]){
+				//printf("%c \n",tracks[t][pos+n]);
+			
+			note[0]=tracks[t][pos+n];
+			note[1]=tracks[t][pos+n];
+			note[2]=tracks[t][pos+n];
+			note[3]=tracks[t][pos+n];
+			}
+			else{
+				note[0]='-';
+				note[1]='-';
+				note[2]='-';
+				note[3]='-';
+			}
 
+
+			if(t==0){
+			snprintf(np,5,"%.4d ",pos+n);
+			gui_text((0 + FONT_CW * (1 + t*5))-FONT_CW,y0 + FONT_CH * (1 + n) + 3,np, screen,0,97,77);
+			}
+			//n % m == n & (m - 1) 
+			//bitwise and to replace modulo
+			if((pos+n)&(16-1)){
+			gui_text(FONT_CW*3 + FONT_CW * (1 + t*5),y0 + FONT_CH * (1 + n) + 3,note, screen,9,255,199);
+			}
+			else{
+			gui_text(FONT_CW*3 + FONT_CW * (1 + t*5),y0 + FONT_CH * (1 + n) + 3,note, screen,9,055,199);	
+			}
+			//gui_text((FONT_CW*3 + FONT_CW * (1 + t*5))+FONT_CW*1,y0 + FONT_CH * (1 + n) + 3,"-", screen,9,255,199);
+			//gui_text((FONT_CW*3 + FONT_CW * (1 + t*5))+FONT_CW*2,y0 + FONT_CH * (1 + n) + 3,"-", screen,9,255,199);
+			//gui_text((FONT_CW*3 + FONT_CW * (1 + t*5))+FONT_CW*3,y0 + FONT_CH * (1 + n) + 3,"-", screen,9,255,199);
+		}
+	}
 	/* Cursors */
-	gui_text(12 + FONT_CW * (6 + (ppos & 0x1f)), y0, "\003", screen,9,255,199);
-	gui_text(12 + FONT_CW * (6 + (ppos & 0x1f)),
-			y0 + FONT_CH * (SSEQ_TRACKS + 1) + 6,
-			"\003", screen,9,255,199);
+	gui_text((FONT_CW*5 * ( 1+(ppos )))-10, y0, "****", screen,9,255,199);
+	gui_text((FONT_CW*5 * ( 1+(ppos )))-17,y0 + FONT_CH * (SSEQ_TRACKS + 1) ,"(   )", screen,9,255,199);
 	//if(editing)
 	//	gui_text(12 + FONT_CW * (6 + (ppos & 0x1f)),
 	//			y0 + FONT_CH * (1 + track) + 3,
@@ -208,7 +266,7 @@ static void draw_main(void)
 	//gui_bar(6, 46, 228, 44, fwc, screen);
 	gui_tempo(tempo);
 	//gui_songpos(0);
-
+	gui_rec();
 	/* Status box */
 	//gui_bar(6, 94, 228, 44, fwc, screen);
 	//gui_status(0, 0, 0);
@@ -216,7 +274,7 @@ static void draw_main(void)
 	/* Song editor */
 	//gui_bar(6, 142, 640 - 12, FONT_CH * (SSEQ_TRACKS + 2) + 12,
 	//		fwc, screen);
-	gui_songedit(0, 0, 0, 0);
+	gui_songedit(pos, track, 0, 0);
 
 	/* Message bar */
 	//gui_bar(6, screen->h - FONT_CH - 12 - 6,
@@ -225,13 +283,40 @@ static void draw_main(void)
 }
 
 
+
 //handle key control
 static void handle_key_ctrl(SDL_Event *ev)
 {
 	switch(ev->key.keysym.sym)
 	{
+	  case SDLK_LEFT:
+	  	track--;
+	  	if(track<0){
+	  		track=15;
+	  	}
+		break;
+	  case SDLK_RIGHT:
+	  	track++;
+	  	if(track>15){
+	  		track=0;
+	  	}
+		break;
+	  case SDLK_UP:
+	  	pos--;
+		break;
+	  case SDLK_DOWN:
+	  	pos++;
+		break;
 	  case SDLK_INSERT:
-	  case SDLK_c:
+	  case SDLK_SPACE:
+	  		playing=!playing;
+	  		break;
+	  case SDLK_i:
+	  	emode=!emode;
+		break;
+	  case SDLK_RETURN:
+	  	pos=0;
+	  	playing=1;
 		break;
 	  case SDLK_x:
 		break;
@@ -241,7 +326,12 @@ static void handle_key_ctrl(SDL_Event *ev)
 		break;
 	  case SDLK_s:
 		break;
-	  case SDLK_n:
+	  case SDLK_1:
+	  	if(emode){
+  			tracks[track][pos+16]='1';
+  			//printf("%c \n",tracks[track][pos+16]);
+  			pos+=1;
+	  	}
 		break;
 	  case SDLK_0:
 	  	tempo++;
@@ -266,9 +356,31 @@ static void breakhandler(int a)
 	die = 1;
 }
 
+void *clock(void *arg)
+{
+    while(1)
+    {	
+    	//make this clock monotonic to try and fix any jitter///also output mraa clock pulse here 
+    	if(playing){
+    		pos+=1;
+    		//60,000 / 100 bpm = 600ms /8 for 32nd notes
+    		//long time = ;
+    		//printf("%d\n", time);
+        
+    	}
+        	usleep(((60000 / tempo)/8)*1000);
+
+    }
+    return 0;
+}
+
 
 int main(int argc, char *argv[])
 {
+
+	//spawn clock threads
+	pthread_t tid;
+	pthread_create(&tid, NULL, &clock, NULL);
 
 	
 	///init SDL
@@ -282,7 +394,7 @@ int main(int argc, char *argv[])
 	signal(SIGINT, breakhandler);
 
 	//2880x1800
-	screen = SDL_SetVideoMode(1440/2, 900/2	, 16, sdlflags);
+	screen = SDL_SetVideoMode(1024, 600	, 8, sdlflags);
 	if(!screen)
 	{
 		fprintf(stderr, "Couldn't open display!\n");
@@ -318,13 +430,16 @@ int main(int argc, char *argv[])
 		int tick = SDL_GetTicks();
 		int dt = tick - last_tick;
 		last_tick = tick;
-		SDL_UpdateRect(screen, 0, 0, 0, 0);
-	    //SDL_FillRect(screen,&screen->clip_rect,SDL_MapRGB(screen->format,0,0,0));
 
-		draw_main();
 		//gui_text( 390, tick/100, "abcdefghijklmnopqrstuvwxyz", screen,(23+34+255+tick)%255,(255-255-1212-tick)%255,tick%255);
+		//printf("%d\n",playing);
+
+		//handle playbkac triggers
+		//
+
 
 		/* Handle GUI events */
+
 		while(SDL_PollEvent(&ev))
 		{
 			switch(ev.type)
@@ -339,7 +454,9 @@ int main(int argc, char *argv[])
 			}
 		}
 
-	
+
+		SDL_UpdateRect(screen, 0, 0, 0, 0);
+		draw_main();	
 		//sdl wait
 		SDL_Delay(10);
 	}
